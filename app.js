ca055b0e77219b15bed0df2a27adc19597568964
ankurshahi80 <shahiankur80@gmail.com>
2022-01-10 23:02:22 -0500

    add CSS and write HTML and Copy CSS to dist folder using async functions

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
+const {writeFile, copyFile} = require('./utils/generate-site');
 const inquirer = require('inquirer');
-const fs = require('fs');
+
 const generatePage = require('./src/page-template');
 
 const mockData = {
@@ -171,19 +172,37 @@ const promptProject = portfolioData => {
     }
   });
 };
-// promptUser()
-// .then(promptProject)
-// .then(portfolioData => {
-//   const pageHTML = generatePage(portfolioData);
+promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse =>{
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
+  });  
+// const pageHTML=generatePage(mockData);
+// fs.writeFile('./dist/index.html',pageHTML, err=>{
+//     if(err) {
+//       console.log(err);
+//       return;
+//     }
+//     console.log('Page created! Check out index.html in this directory to see it!');
 
-// // fs.writeFile('index.html',pageHTML, err=>{
-// //   if(err) throw err;
-// //   console.log('Portfolio complete! Check out index.html to see the output!')
-// // });
-// });
-
-const pageHTML=generatePage(mockData);
-fs.writeFile('index.html',pageHTML, err=>{
-    if(err) throw err;
-    console.log('Portfolio complete! Check out index.html to see the output!')
-  });
+//     fs.copyFile('./src/style.css', './dist/style.css', err => {
+//       if(err) {
+//         console.log(err);
+//         return;
+//       }
+//       console.log('Style sheet copied successfully!');
+//     });
+//   });
